78a8db2266925aa9aff3dd824114efa13001d2a6
eliasleipola <Elias.Leipola@metropolia.fi>
2023-11-15 11:07:05 +0200

    Added an aside component and modified ComponentStyle.css

--- src/pages/Page1/Page1.tsx
+++ src/pages/Page1/Page1.tsx
@@ -1,13 +1,17 @@
+import AsideComponent from '@/components/asideComponent';
 import DivComponent from '@/components/divComponent';
 
 function Page1() {
   return (
     <>
-      <h1>Test page</h1>
       <DivComponent>
         <h1 style={{ color: 'black' }}>This is a test header for the div element!</h1>
         <p style={{ color: 'black' }}>This text paragraph is also meant for testing!</p>
       </DivComponent>
+      <AsideComponent>
+        <h1 style={{ color: 'black' }}>Test page</h1>
+        <p style={{ color: 'black' }}>This paragraph tests the aside element.</p>
+      </AsideComponent>
     </>
   );
 }
